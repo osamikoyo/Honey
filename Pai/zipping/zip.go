36c765db15b5bf6ee5d386b11b557d2bf7acaf17
osamikoyo <v.pylayev@mail.ru>
2024-10-18 20:07:05 +0500

    zip

--- Pai/zipping/zip.go
+++ Pai/zipping/zip.go
@@ -1,1 +1,52 @@
 package zipping
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func GoZip(source string, tagret string) error {
+
+	zipFile, err := os.Create(tagret)
+    if err != nil {
+        return err
+    }
+    defer zipFile.Close()
+
+    writer := zip.NewWriter(zipFile)
+    defer writer.Close()
+
+    err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+
+        if info.IsDir() {
+            return nil 
+        }
+
+        relPath, err := filepath.Rel(source, path)
+        if err != nil {
+            return err
+        }
+
+        zipEntry, err := writer.Create(relPath)
+        if err != nil {
+            return err
+        }
+
+        file, err := os.Open(path)
+        if err != nil {
+            return err
+        }
+        defer file.Close()
+
+        _, err = io.Copy(zipEntry, file)
+        return err
+    })
+
+    return err
+
+}
